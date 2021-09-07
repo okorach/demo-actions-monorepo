dce67176c60196fe734ae44c05656c504a12bbe7
Olivier Korach <olivier.korach@gmail.com>
2021-09-07 13:54:53 +0200

    Fixes #40

--- comp-maven/build.sh
+++ comp-maven/build.sh
@@ -2,7 +2,7 @@
 
 # Load common environment
 
-mvn clean install -P coverage-per-test \
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report \
    -Dmaven.test.failure.ignore=true \
    sonar:sonar \
    -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN \
